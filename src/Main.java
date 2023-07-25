183d30a5371afaae80a2352824c1b2c2d4ac5d31
tendryAxel <128225840+tendryAxel@users.noreply.github.com>
2023-07-25 08:44:20 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.*;
-import java.util.Scanner;
+import java.util.List;
 
 
 public class Main {
 
-    public static void query(String sql, String user, String database, String host, String port, String password) throws IOException, InterruptedException {
-        String command = String.format("psql -U %s -d %s -h %s -p %s -c \"%s\"", user, database, host, port, sql);
-        ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", command);
-        processBuilder.environment().put("PGPASSWORD", password);
+    public static String query(String sql, String user, String database, String host, String port) throws IOException, InterruptedException {
+        List<String> command = List.of(String.format("psql -U %s -d %s -h %s -p %s -c \"%s\"", user, database, host, port, sql).split(" "));
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        System.out.println(sql);
 
         Process process = processBuilder.start();
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -19,19 +17,16 @@ public class Main {
         while ((line = reader.readLine()) != null){
             System.out.println(line);
         }
+
+        int exitCode = process.waitFor();
+        return ("Exit code: " + exitCode);
     }
 
     public static void main(String[] args) throws IOException, InterruptedException, NullPointerException {
         String USER = "postgres";
-        String PASSWORD = "password";
         String DATABASE = "javatest";
         String HOST = "localhost";
         String PORT = "5432";
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Changer le mot de passe (password) ?");
-        String tmp = in.nextLine();
-        if (!tmp.equals("")) { PASSWORD = tmp;}
 
         if (args.length == 0) {
             System.out.println("Il manque des arguments pour l'execution");
@@ -43,13 +38,13 @@ public class Main {
                     if (args.length != 2) {
                         System.out.println("Il manque des arguments pour l'execution");
                     } else {
-                        String sql = String.format("INSERT INTO connection(first_name) VALUES (%s)", args[1]);
-                        query(sql, USER, DATABASE, HOST, PORT, PASSWORD);
+                        String sql = String.format("INSERT INTO connection (first_name) VALUES ('%s');", args[1]);
+                        query(sql, USER, DATABASE, HOST, PORT);
                     }
                 }
                 case "showAllConnection" -> {
                     String sql = "SELECT * FROM connection;";
-                    query(sql, USER, DATABASE, HOST, PORT, PASSWORD);
+                    query(sql, USER, DATABASE, HOST, PORT);
                 }
                 default -> {
                     System.out.println("Unknow command : " + args[0]);
